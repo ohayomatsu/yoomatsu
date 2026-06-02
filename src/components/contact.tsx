8882e7da2219d762dd2170dab4ada1e24a85ce71
Lucas Matsumura <lucassmatsumura@gmail.com>
2026-06-02 04:11:03 +0000

    no texto "Ficou interessado? só chamar.", troque o . no final para uma !

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -8,7 +8,7 @@ export function Contact() {
     <section id="contact" className="pt-4 pb-32 px-6 max-w-[500px] mx-auto">
       <div className="flex flex-col items-center text-center gap-10">
         <h2 className="text-[1.2rem] font-headline font-bold tracking-tight glow-text leading-tight">
-          Ficou interessado? <br /> só chamar.
+          Ficou interessado? <br /> só chamar!
         </h2>
         
         <div className="flex items-center justify-center gap-6">
