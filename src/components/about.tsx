27c7a7942f09e5dc2df74c95aa1ff246a6da8262
Lucas Matsumura <lucassmatsumura@gmail.com>
2026-06-02 22:13:46 +0000

    Na seção "SOBRE", adicione negrito nas palavras sublinhadas "After Effec

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -31,7 +31,7 @@ export function About() {
           
           <div className="text-[0.95rem] text-foreground/60 leading-relaxed">
             <p>
-              Opa, eu sou o Matsu, trabalho com edição de vídeo e motion design. Sou especializado em <u>After Effects</u> e <u>Premiere Pro</u> há mais de 8 anos, começando na época de ouro das Intros.
+              Opa, eu sou o Matsu, trabalho com edição de vídeo e motion design. Sou especializado em <strong><u>After Effects</u></strong> e <strong><u>Premiere Pro</u></strong> há mais de 8 anos, começando na época de ouro das Intros.
             </p>
           </div>
 
